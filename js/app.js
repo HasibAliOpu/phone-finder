e2381a667396253c9099c192e9524e5f6cc0e36c
H_A _Opu <wwwhasib4@gmail.com>
2022-03-01 00:15:31 +0600

    display single phone details

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const displayPhones = phones => {
     const displayPhones = document.getElementById('display-phones');
     displayPhones.innerHTML = '';
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -21,10 +21,39 @@ const displayPhones = phones => {
                 <div class="card-body">
                 <h5>${phone.phone_name}</h5>
                 <p>${phone.brand}</p>
-                <button type="button" class="btn btn-info">Details</button>
+                <button onclick="getSinglePhoneDetails('${phone.slug}')" type="button" class="btn btn-info">Details</button>
                 </div>
               </div>
         `;
         displayPhones.appendChild(div)
     });
+}
+
+const getSinglePhoneDetails = phoneId => {
+    const url =`https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySinglePhoneDetails(data.data))
+}
+
+const displaySinglePhoneDetails = singlePhone => {
+    // console.log(singlephone);
+    console.log(singlePhone.mainFeatures);
+    const  singlePhoneDetails = document.getElementById('single-phone-details')
+    singlePhoneDetails.innerHTML = `
+    <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${singlePhone.image}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${singlePhone.name}</h5>
+        <p>Chipset: ${singlePhone.mainFeatures.chipSet}</p>
+        <p>Storage: ${singlePhone.mainFeatures.storage}</p>
+        <p>Display-Size: ${singlePhone.mainFeatures.displaySize}</p>
+        <p class="card-text"><small class="text-muted">${singlePhone.releaseDate}</small></p>
+      </div>
+    </div>
+  </div>
+    `
 }
