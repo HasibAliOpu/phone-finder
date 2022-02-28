67b35aae39581a9cd73c2e1bf49d943f4bec53dc
H_A _Opu <wwwhasib4@gmail.com>
2022-02-28 23:22:45 +0600

    display phone img on cards

--- js/app.js
+++ js/app.js
@@ -9,7 +9,20 @@ const loadPhones = () =>{
 }
 
 const displayPhones = phones => {
+    const displayPhones = document.getElementById('display-phones');
     phones.forEach(phone => {
         console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img  src="${phone.image}" class="card-img-top w-50" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+        `;
+        displayPhones.appendChild(div)
     });
 }
