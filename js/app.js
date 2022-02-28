052c96aadfbe7823b65b180b671fc793d55d7999
H_A _Opu <wwwhasib4@gmail.com>
2022-02-28 23:39:05 +0600

    added phone img,name,brand and details btn

--- js/app.js
+++ js/app.js
@@ -10,16 +10,18 @@ const loadPhones = () =>{
 
 const displayPhones = phones => {
     const displayPhones = document.getElementById('display-phones');
+    displayPhones.innerHTML = '';
     phones.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-                <img  src="${phone.image}" class="card-img-top w-50" alt="...">
+        <div class="card h-100 w-75">
+                <img  src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5>${phone.phone_name}</h5>
+                <p>${phone.brand}</p>
+                <button type="button" class="btn btn-info">Details</button>
                 </div>
               </div>
         `;
