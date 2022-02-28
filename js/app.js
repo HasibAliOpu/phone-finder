209dc2c6e90a420e5150367476d0488486fe77b0
H_A _Opu <wwwhasib4@gmail.com>
2022-02-28 23:12:37 +0600

    getting search phone api and loop throw

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ const loadPhones = () =>{
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
      searchField.value = '';
+     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+     fetch(url)
+     .then(res => res.json())
+     .then(data => displayPhones(data.data))
+}
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+        console.log(phone);
+    });
 }
